236cb39aab81ec69e8bfab7a64e85bed97e457e5
Kanti Lal <kantilal@yudiz.com>
2023-10-06 14:43:36 +0530

    episode-O1

---
+++ App.js
@@ -0,0 +1,25 @@
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello World From React."
+);
+
+const parent = React.createElement(
+  "div",
+  {
+    id: "parent",
+  },
+  [
+    React.createElement("div", { id: "child" }, [
+      React.createElement("h1", {}, "I'm in h1 tag"),
+      React.createElement("h2", {}, "I'm in h2 tag"),
+    ]),
+    React.createElement("div", { id: "child2" }, [
+      React.createElement("h1", {}, "I'm in h1 tag"),
+      React.createElement("h2", {}, "I'm in h2 tag"),
+    ]),
+  ]
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
