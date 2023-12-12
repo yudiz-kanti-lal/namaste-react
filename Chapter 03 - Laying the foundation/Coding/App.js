7d227a266c101b61d02373e724cc0c30f2e31f49
Kanti Lal <kantilal@yudiz.com>
2023-12-12 17:35:02 +0530

    chapter: 3 code updated

---
+++ Chapter 03 - Laying the foundation/Coding/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+
+
+const title =  (
+  <h1 className='head' tabIndex='5'>
+  Namaste-react using jsx
+  </h1>
+)
+
+const HeadingComponent = () => (
+  <div id='container'>
+ {title}
+  <h2>
+  {console.log("this is console")}
+  </h2>
+  <h1>Namaste-react functional component</h1>
+  </div>
+) 
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(<HeadingComponent/>);
